1b46f53aef7ef764732aba90371eab6550e0b648
derek og <derekzyl@gmail.com>
2023-07-06 08:09:59 +0100

    added new stuffk

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,12 +37,28 @@ const index_user_1 = __importDefault(require("./src/api/user/index.user"));
 const route_t_1 = __importDefault(require("./src/api/test/main_test/route.t"));
 const route_notification_1 = __importDefault(require("./src/api/notification/main_notification/route.notification"));
 const route_general_1 = __importDefault(require("./src/api/general_factory/route.general"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const dotenv = __importStar(require("dotenv"));
+const rate_limiter_1 = __importDefault(require("./src/utilities/rate_limiter"));
+const compression_1 = __importDefault(require("compression"));
+dotenv.config();
 const app = (0, express_1.default)();
+// security adding and implemnentations
+app.use((0, cors_1.default)());
+app.options("*", (0, cors_1.default)());
+app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+// gzip compression
+app.use((0, compression_1.default)());
 app.get("/home", (req, res) => {
     res.status(200).json({ hey: "welcome to jenny glow backend" });
 });
+// limit repeated failed requests to auth endpoints
+if (process.env.environment === "production") {
+    app.use("/api/v1/auth", rate_limiter_1.default);
+}
 app.use("/test", route_t_1.default);
 app.use("/admin", index_admin_1.default);
 app.use("/auth", route_auth_1.default);
--- main.ts
+++ main.ts
@@ -11,15 +11,32 @@ import userRouter from "./src/api/user/index.user";
 import testRouter from "./src/api/test/main_test/route.t";
 import notificationRouter from "./src/api/notification/main_notification/route.notification";
 import generalRouter from "./src/api/general_factory/route.general";
+import cors from "cors";
+import helmet from "helmet";
+import * as dotenv from "dotenv";
+import authLimiter from "./src/utilities/rate_limiter";
+
+import compression from "compression";
+dotenv.config();
 
 const app: Express = express();
 
+// security adding and implemnentations
+app.use(cors());
+app.options("*", cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compression());
 app.get("/home", (req, res) => {
-    res.status(200).json({hey:"welcome to jenny glow backend"})
-})
-    
+  res.status(200).json({ hey: "welcome to jenny glow backend" });
+});
+// limit repeated failed requests to auth endpoints
+if (process.env.environment === "production") {
+  app.use("/api/v1/auth", authLimiter);
+}
 app.use("/test", testRouter);
 app.use("/admin", adminRouter);
 app.use("/auth", authRouter);
